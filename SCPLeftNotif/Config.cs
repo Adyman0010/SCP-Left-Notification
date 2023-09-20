29316cec750ff56feea0f265fa064f8decef9b10
Adyman0010 <adyman0010@gmail.com>
2023-09-20 19:04:41 +0200

    FINAL FIX (I'm flippin stupid)

--- SCPLeftNotif/Config.cs
+++ SCPLeftNotif/Config.cs
@@ -8,10 +8,7 @@ namespace SCPLeftNotif
         public bool IsEnabled { get; set; } = true;
 
         public bool Debug { get; set; } = false;
-
-        [Description("Is ScpLeftNotif enabled (default = true)")]
-        public bool IsScpLeftNotifEnabled { get; set; } = true;
-
+        
         [Description("What message should brodcast to admins, when SCP leaves")]
         public string AdminMessage { get; set; } = "A Player left as a SCP! Check your console by pressing ;";
     }
