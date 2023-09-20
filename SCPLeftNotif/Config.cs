cb3a1ba11caf07b08cd4f9638665e4aae98df28d
Adyman0010 <139592888+Adyman0010@users.noreply.github.com>
2023-09-20 18:33:09 +0200

    Files Upload

---
+++ SCPLeftNotif/Config.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace SCPLeftNotif
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public bool Debug { get; set; } = false;
+
+        [Description("Is ScpLeftNotif enabled (default = true)")]
+        public bool IsScpLeftNotifEnabled { get; set; } = true;
+
+        [Description("What message should brodcast to admins, when SCP leaves")]
+        public string AdminMessage { get; set; } = "A Player left as a SCP! Check your console by pressing ;";
+    }
+}
---
+++ SCPLeftNotif/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+
+namespace SCPLeftNotif
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "Adyman0010";
+
+        public override string Name { get; } = "SCPLeftNotif";
+        
+        public override string Prefix => Name;
+
+        public static Plugin Instance;
+        
+        public override void OnEnabled()
+        {
+            Instance = this;
+
+            Exiled.Events.Handlers.Player.Left += OnScpLeft;
+            
+            base.OnEnabled();
+        }
+        
+        public override void OnDisabled()
+        {
+            Instance = null;
+
+            Exiled.Events.Handlers.Player.Left -= OnScpLeft;
+            
+            base.OnDisabled();
+        }
+        
+        public void OnScpLeft(LeftEventArgs ev)
+        {
+            if ( ev.Player.IsScp && Instance.Config.IsScpLeftNotifEnabled )
+            {
+                foreach (Player player in Player.List)
+                {
+                    if (player.RemoteAdminAccess)
+                    {
+                        player.ShowHint(Instance.Config.AdminMessage, 10f);
+                        player.SendConsoleMessage($"Player {ev.Player.DisplayNickname} his/her STEAMID: {ev.Player.UserId} has left as a SCP, his/her role was {ev.Player.Role.Type}", "yellow");
+                        Log.Info("SCP Left the server! A message was sent to all available staff");
+                    }
+                }
+            }
+        }
+    }
+}
